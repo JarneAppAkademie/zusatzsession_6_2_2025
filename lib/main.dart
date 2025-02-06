0144a21a89406a6d121619af36dac7ee998ee64b
Jarne Jüchser <jarne.juechser@web.de>
2025-02-06 13:41:59 +0100

    reverded changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,45 +4,17 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  List<List<dynamic>> todos = [
-    ["Hallo", false]
-  ];
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primaryColor: Colors.red,
-          appBarTheme: AppBarTheme(
-            centerTitle: true,
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle()),
-          ),
-          
       home: Scaffold(
-          body: ListView.builder(
-              itemCount: todos.length,
-              itemBuilder: (context, index) {
-                return Row(
-                  children: [
-                    Text(todos[index][0]),
-                    Checkbox(
-                        value: todos[index][1],
-                        onChanged: (val) {
-                          setState(() {
-                            todos[index][1] = val;
-                          });
-                        })
-                  ],
-                );
-              })),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
     );
   }
 }
